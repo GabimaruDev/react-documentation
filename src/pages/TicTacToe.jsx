cec86d3c2f77a7770c0ad97e5aa1d7472dc1df03
Gabimaru <goukilya5@gmail.com>
2024-10-01 16:10:55 +0700

    feat: сделал рабочие "Крестики-Нолики"

--- src/pages/TicTacToe.jsx
+++ src/pages/TicTacToe.jsx
@@ -1,20 +1,79 @@
+import { useState } from "react";
+
+
+function Square({ value, onSquareClick }) {
+    return <button onClick={onSquareClick}>{value}</button>
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+    
+    return null
+}
+
+
 const TicTacToe = () => {
+    const [xIsNext, setXIsNext] = useState(true)
+    const [squares, setSquares] = useState(Array(9).fill(null))
+
+    const winner = calculateWinner(squares)
+
+    function handleClick(i) {
+        if (winner || squares[i]) {
+            return
+        }
+        const nextSquares = squares.slice()
+
+        if (xIsNext) {
+            nextSquares[i] = "X"
+        } else {
+            nextSquares[i] = "O"
+        }
+        setSquares(nextSquares)
+        setXIsNext(!xIsNext)
+    }
+
+    let status
+    if (winner) {
+        status = "Победитель: " + winner
+    } else {
+        status = "Следующий ход: " + (xIsNext ? "X" : "O")
+    }
+
+
     return (
         <div className="max-w-fit mx-auto h-[calc(100svh-4rem)] flex flex-col justify-center">
+            <div className="mx-auto text-xl py-2">{status}</div>
             <div className="board-row">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
             </div>
             <div className="board-row">
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
             </div>
             <div className="board-row">
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
             </div>
         </div>
     );
